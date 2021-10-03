3af54a541022bc1a74e7bddf9d0759bc428a65d2
Adam Rolczyk <adam.rolczyk@syzygy.pl>
2021-10-03 08:24:48 +0200

    Fix linting issues

--- generator/index.js
+++ generator/index.js
@@ -1,17 +1,7 @@
-/* eslint-disable no-console */
 const sW = 80;
 const dim = 501;
 const res = 100;
 const cP = ['#0f0f0f'];
-
-function setup() {
-  createCanvas(dim, dim, SVG);
-  // noFill();
-  // background('#0f0f0f');
-  for (let i = 0; i < 1; i++) {
-    drawShapes();
-  }
-}
 
 const drawShapes = () => {
   strokeCap(PROJECT);
@@ -21,53 +11,53 @@ const drawShapes = () => {
       const xEdge = dim - res * 2 <= x;
       const yEdge = dim - res * 2 <= y;
       const d = [
-        /* 0 */ [x, y - res, '⬆'],
-        /* 1 */ [x + res, y - res, '↗'],
-        /* 2 */ [x + res, y, '➡'],
-        /* 3 */ [x + res, y + res, '↘'],
-        /* 4 */ [x, y + res, '⬇'],
-        /* 5 */ [x - res, y + res, '↙'],
-        /* 6 */ [x - res, y, '⬅'],
-        /* 7 */ [x - res, y - res, '↖'],
+        [x, y - res, '⬆'],
+        [x + res, y - res, '↗'],
+        [x + res, y, '➡'],
+        [x + res, y + res, '↘'],
+        [x, y + res, '⬇'],
+        [x - res, y + res, '↙'],
+        [x - res, y, '⬅'],
+        [x - res, y - res, '↖'],
       ];
       const directions = [];
-      if (x === res && y === res) { // 234
+      if (x === res && y === res) {
         directions.push(d[2]);
         directions.push(d[4]);
         // directions.push(d[3]);
-      } else if (x === res && (y > res && !yEdge)) { // 01234
+      } else if (x === res && (y > res && !yEdge)) {
         directions.push(d[0]);
         directions.push(d[2]);
         directions.push(d[4]);
         // directions.push(d[1]);
         // directions.push(d[3]);
-      } else if ((x > res && !xEdge) && y === res) { // 23456
+      } else if ((x > res && !xEdge) && y === res) {
         directions.push(d[2]);
         directions.push(d[4]);
         directions.push(d[6]);
         // directions.push(d[3]);
         // directions.push(d[5]);
-      } else if (xEdge && y === res) { // 456
+      } else if (xEdge && y === res) {
         directions.push(d[4]);
         directions.push(d[6]);
         // directions.push(d[5]);
-      } else if (xEdge && (y > res && !yEdge)) { // 04567
+      } else if (xEdge && (y > res && !yEdge)) {
         directions.push(d[0]);
         directions.push(d[4]);
         directions.push(d[6]);
         // directions.push(d[5]);
         // directions.push(d[7]);
-      } else if (xEdge && yEdge) { // 067
+      } else if (xEdge && yEdge) {
         directions.push(d[0]);
         directions.push(d[6]);
         // directions.push(d[7]);
-      } else if ((x > res && !xEdge) && yEdge) { // 01267
+      } else if ((x > res && !xEdge) && yEdge) {
         directions.push(d[0]);
         directions.push(d[2]);
         directions.push(d[6]);
         // directions.push(d[1]);
         // directions.push(d[7]);
-      } else if (x === res && yEdge) { // 012
+      } else if (x === res && yEdge) {
         directions.push(d[0]);
         directions.push(d[2]);
         // directions.push(d[1]);
@@ -82,22 +72,25 @@ const drawShapes = () => {
         // directions.push(d[7]);
       }
       const randDir = directions[Math.floor(Math.random() * directions.length)];
-      if (randDir[0] === x && randDir[1] === y) {
-        console.log("dot");
-      } else {
+      if (!(randDir[0] === x && randDir[1] === y)) {
         stroke(cP[Math.floor(Math.random() * cP.length)]);
-        line(
-          x,
-          y,
-          randDir[0],
-          randDir[1],
-        );
-        // filter(BLUR, 5);
+        line(x, y, randDir[0], randDir[1]);
       }
     }
   }
 };
 
+// eslint-disable-next-line no-unused-vars
+function setup() {
+  // 'SVG' var comes from p5.js-svg package
+  // eslint-disable-next-line no-undef
+  createCanvas(dim, dim, SVG);
+  for (let i = 0; i < 1; i += 1) {
+    drawShapes();
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
 function keyPressed() {
   const dateObj = new Date();
   const month = dateObj.getUTCMonth() + 1;
